9fa4cbb4ba01b69940e32e20ee2357734b0242d3
ben awad <benawad97@gmail.com>
2017-10-19 22:09:07 -0500

    add good styles

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -8,29 +8,61 @@ const ChannelWrapper = styled.div`
   color: #958993;
 `;
 
-const channel = ({ id, name }) => <li key={`channel-${id}`}># {name}</li>;
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
 
-const user = ({ id, name }) => <li key={`user-${id}`}>{name}</li>;
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
+
+const paddingLeft = 'padding-left: 10px';
+
+const SideBarListItem = styled.li`
+  ${paddingLeft};
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const SideBarListHeader = styled.li`${paddingLeft};`;
+
+const PushLeft = styled.div`${paddingLeft};`;
+
+const Green = styled.span`color: #38978d;`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : '○');
+
+const channel = ({ id, name }) => <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>;
+
+const user = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
+);
 
 export default ({
   teamName, username, channels, users,
 }) => (
   <ChannelWrapper>
-    <div>
-      {teamName}
+    <PushLeft>
+      <TeamNameHeader>{teamName}</TeamNameHeader>
       {username}
-    </div>
+    </PushLeft>
     <div>
-      <ul>
-        <li>Channels</li>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
         {channels.map(channel)}
-      </ul>
+      </SideBarList>
     </div>
     <div>
-      <ul>
-        <li>Direct Messages</li>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
         {users.map(user)}
-      </ul>
+      </SideBarList>
     </div>
   </ChannelWrapper>
 );
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -8,10 +8,35 @@ const TeamWrapper = styled.div`
   color: #958993;
 `;
 
-const team = ({ id, letter }) => <li key={`team-${id}`}>{letter}</li>;
+const TeamList = styled.ul`
+  width: 100%;
+  padding-left: 0px;
+  list-style: none;
+`;
+
+const TeamListItem = styled.li`
+  height: 50px;
+  width: 50px;
+  background-color: #676066;
+  color: #fff;
+  margin: auto;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  border-radius: 11px;
+  &:hover {
+    border-style: solid;
+    border-width: thick;
+    border-color: #767676;
+  }
+`;
+
+const team = ({ id, letter }) => <TeamListItem key={`team-${id}`}>{letter}</TeamListItem>;
 
 export default ({ teams }) => (
   <TeamWrapper>
-    <ul>{teams.map(team)}</ul>
+    <TeamList>{teams.map(team)}</TeamList>
   </TeamWrapper>
 );
