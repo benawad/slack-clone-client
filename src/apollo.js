f61169c32372bb374bb189baa40ab6cf50a31bd9
ben awad <benawad97@gmail.com>
2017-11-11 16:12:20 -0600

    upgrade packages and get headers from response

--- src/apollo.js
+++ src/apollo.js
@@ -1,62 +1,65 @@
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { setContext } from 'apollo-link-context';
-import { ApolloLink, split } from 'apollo-link';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { ApolloLink, split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
-const httpLink = createHttpLink({ uri: 'http://localhost:8081/graphql' });
+const httpLink = createHttpLink({ uri: "http://localhost:8081/graphql" });
 
 const middlewareLink = setContext(() => ({
   headers: {
-    'x-token': localStorage.getItem('token'),
-    'x-refresh-token': localStorage.getItem('refreshToken'),
-  },
+    "x-token": localStorage.getItem("token"),
+    "x-refresh-token": localStorage.getItem("refreshToken")
+  }
 }));
 
 const afterwareLink = new ApolloLink((operation, forward) => {
-  const { headers } = operation.getContext();
+  return forward(operation).map(response => {
+    const { response: { headers } } = operation.getContext();
+    if (headers) {
+      const token = headers.get("x-token");
+      const refreshToken = headers.get("x-refresh-token");
 
-  if (headers) {
-    const token = headers.get('x-token');
-    const refreshToken = headers.get('x-refresh-token');
+      if (token) {
+        localStorage.setItem("token", token);
+      }
 
-    if (token) {
-      localStorage.setItem('token', token);
+      if (refreshToken) {
+        localStorage.setItem("refreshToken", refreshToken);
+      }
     }
 
-    if (refreshToken) {
-      localStorage.setItem('refreshToken', refreshToken);
-    }
-  }
-
-  return forward(operation);
+    return response;
+  });
 });
 
-const httpLinkWithMiddleware = afterwareLink.concat(middlewareLink.concat(httpLink));
+const httpLinkWithMiddleware = afterwareLink.concat(
+  middlewareLink.concat(httpLink)
+);
 
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:8081/subscriptions',
+  uri: "ws://localhost:8081/subscriptions",
   options: {
     reconnect: true,
     connectionParams: {
-      token: localStorage.getItem('token'),
-      refreshToken: localStorage.getItem('refreshToken'),
-    },
-  },
+      token: localStorage.getItem("token"),
+      refreshToken: localStorage.getItem("refreshToken")
+    }
+  }
 });
 
 const link = split(
   ({ query }) => {
     const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
+    return kind === "OperationDefinition" && operation === "subscription";
   },
   wsLink,
-  httpLinkWithMiddleware,
+  httpLinkWithMiddleware
 );
 
 export default new ApolloClient({
   link,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
